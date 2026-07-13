6c59f3b3c6054b42840aed57342bea76fc3e208c
rohit <rohitchauhan00371@gmail.com>
2026-07-13 13:27:42 +0530

    commited topseals

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Banner from "@/components/Banner";
 import Topals from "@/components/Topals";
+import Topdeals from "@/components/Topdeals";
 import Image from "next/image";
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
     <>
     <Banner />
     <Topals/>
+    <Topdeals/>
     </>
   );
 }
